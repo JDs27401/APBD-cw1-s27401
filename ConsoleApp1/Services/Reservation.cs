7cf860c2a2c9df450c88e42f2d372a5fc5b28094
JDs27401 <s27401@pjwstk.edu.pl>
2026-03-24 23:00:45 +0100

    Created Reservation class with some initial code
    Created the fundation for reservation class. Methods and enum to be done.

--- ConsoleApp1/Services/Reservation.cs
+++ ConsoleApp1/Services/Reservation.cs
@@ -1,6 +1,22 @@
-﻿namespace ConsoleApp1.Services;
+﻿using ConsoleApp1.Models;
+
+namespace ConsoleApp1.Services;
 
 public class Reservation
 {
+    public static Dictionary<string, DeviceBase> Reservations = new Dictionary<string, DeviceBase>();
+    private static int _mainId = 1;
+    public string Id { get; } = "R" + _mainId++;
     
+    public ICollection<DeviceBase> Devices { get; } = new List<DeviceBase>();
+
+    public Person Owner { get; }
+
+    private Reservation(DeviceBase[] devices, Person person, DateTime reservationDate, DateTime reservationEndDate)
+    {
+        //@todo implement a constructor with assignments for all variables
+    }
+    
+    //@todo implement functions for creating and resolving reservation
+    //@todo add enum with status for active and finished reservations
 }
