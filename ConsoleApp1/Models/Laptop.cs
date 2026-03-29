8df9a914cd67f33701c84c33a3eff5fbd6034b36
JDs27401 <s27401@pjwstk.edu.pl>
2026-03-29 23:12:23 +0200

    Added Laptop class

---
+++ ConsoleApp1/Models/Laptop.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleApp1.Models;
+
+public class Laptop : DeviceBase
+{
+    private float _batterySize;
+    public float BatterySize
+    {
+        get => _batterySize;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("value must be > 0");
+            } 
+            _batterySize = value;
+        }
+    }
+
+    private float _screenSize;
+    public float ScreenSize
+    {
+        get => _screenSize;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("value must be > 0");
+            }
+            _screenSize = value;
+        }
+    }
+
+    public Laptop(float batterySize, float screenSize, string name) : base(name)
+    {
+        BatterySize = batterySize;
+        ScreenSize = screenSize;
+    }
+}
